6c02f231d7650d04152bae12b4ebfcddc63d9dbf
Marius Debussche <marius.debussche@gmail.com>
2024-12-20 17:16:13 +0100

    rtic with uart interrupt

--- src/bin/minimal.rs
+++ src/bin/minimal.rs
@@ -2,52 +2,80 @@
 #![no_std]
 #![feature(type_alias_impl_trait)]
 
-use test_app as _; // global logger + panicking-behavior + memory layout
+use mseq_hardware as _; // global logger + panicking-behavior + memory layout
 
-// TODO(7) Configure the `rtic::app` macro
 #[rtic::app(
-    // TODO: Replace `some_hal::pac` with the path to the PAC
-    device = some_hal::pac,
+    device = stm32f4xx_hal::pac,
     // TODO: Replace the `FreeInterrupt1, ...` with free interrupt vectors if software tasks are used
     // You can usually find the names of the interrupt vectors in the some_hal::pac::interrupt enum.
-    dispatchers = [FreeInterrupt1, ...]
+    dispatchers = [TIM3],
+    peripherals = true,
 )]
-mod app {
-    // Shared resources go here
+mod app {   
+    use rtic_monotonics::systick::prelude::*;
+    use stm32f4xx_hal::{
+        interrupt,
+        pac::{self, USART1},
+        prelude::*,
+        rcc::AHB1,
+        serial::{
+            self, config::{DmaConfig, StopBits::STOP1}, Config, Serial
+        },
+    };
+    
+    systick_monotonic!(Mono, 100);
+
     #[shared]
     struct Shared {
-        // TODO: Add resources
     }
 
-    // Local resources go here
     #[local]
     struct Local {
-        // TODO: Add resources
+        serial: Serial<USART1>
     }
 
     #[init]
     fn init(cx: init::Context) -> (Shared, Local) {
         defmt::info!("init");
+        
+        // Midi connection
+        let rcc = cx.device.RCC.constrain();
+        let clocks = rcc.cfgr.freeze();
+        let gpioa = cx.device.GPIOA.split();
+        let rx_1 = gpioa.pa10.into_alternate();
+        let tx_1 = gpioa.pa9.into_alternate();
 
-        // TODO setup monotonic if used
-        // let sysclk = { /* clock setup + returning sysclk as an u32 */ };
-        // let token = rtic_monotonics::create_systick_token!();
-        // rtic_monotonics::systick::Systick::new(cx.core.SYST, sysclk, token);
+        let mut serial = Serial::new(
+            cx.device.USART1,
+            (tx_1, rx_1),
+            Config::default()
+                .baudrate(31250.bps())
+                .wordlength_8()
+                .parity_none()
+                .stopbits(STOP1)
+                .dma(DmaConfig::None),
+            &clocks,
+        ).unwrap();
 
+        serial.listen(serial::Event::RxNotEmpty);
 
+        // Timer
+        Mono::start(cx.core.SYST, 12_000_000);
+
+        // Start
         task1::spawn().ok();
+
+        rtic::pend(interrupt::USART1);
 
         (
             Shared {
-                // Initialization of shared resources go here
             },
             Local {
-                // Initialization of local resources go here
+                serial
             },
         )
     }
 
-    // Optional idle, can be removed if not needed.
     #[idle]
     fn idle(_: idle::Context) -> ! {
         defmt::info!("idle");
@@ -57,9 +85,21 @@ mod app {
         }
     }
 
-    // TODO: Add tasks
+    // Sleep demo task
     #[task(priority = 1)]
     async fn task1(_cx: task1::Context) {
         defmt::info!("Hello from task1!");
+        Mono::delay(5000.millis()).await;
+        defmt::info!("Hello after wait!");
+    }
+
+    // Midi interrupt
+    #[task(binds = USART1, priority = 2, local=[serial])]
+    fn midi_int(cx: midi_int::Context) {
+        let serial = cx.local.serial;
+        match serial.read() {
+            Ok(b) => defmt::info!("Received: {}", b),
+            Err(_) => defmt::info!("Serial is empty"),
+        }
     }
 }
